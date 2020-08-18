bde4b6503a6ec94668bf45045afb1a76e871355a
GreenHornedOwl <daniel.voicu.dev@gmail.com>
2020-08-18 18:08:38 +0300

    update

--- src/components/styleguide/buttons.js
+++ src/components/styleguide/buttons.js
@@ -1,69 +1,73 @@
-const darkThemedButtons =["brand", "accent", "buy", "dark", "muted"];
-const lightThemedButtons = [];
+const buttons =["brand", "accent", "buy", "dark", "muted", "neutral","neutral-light","light"];
+const lightThemedButtons = ["neutral", "neutral-light","light"];
 const themedButtons = [
-  {"label":"Dark Buttons", "value": darkThemedButtons},
-  {"label":"Light Buttons", "value" : lightThemedButtons}
+  {"label":"Default Buttons", "value": buttons},
+  // {"label":"Light Buttons", "value" : lightThemedButtons}
 ];
 const themedButtonsWithIcons = [
-  {"label":"Dark Buttons With Icons", "value": darkThemedButtons},
-  {"label":"Light Buttons With Icons", "value" : lightThemedButtons}
+  {"label":"Buttons With Icons", "value": buttons},
+  // {"label":"Light Buttons With Icons", "value" : lightThemedButtons}
 ]
 const themedButtonsOnlyIcons = [
-  {"label":"Dark Buttons With Icons", "value": darkThemedButtons},
-  {"label":"Light Buttons With Icons", "value" : lightThemedButtons}
+  {"label":"Icon Buttons with Background/Border", "value": buttons},
+  // {"label":"Light Buttons With Icons", "value" : lightThemedButtons}
 ]
 const themedButtonsOnlyIconsColor = [
-  {"label":"Icons", "value": darkThemedButtons},
+  {"label":"Icons Only Buttons", "value": buttons},
   // {"label":"Light Buttons With Icons", "value" : lightThemedButtons}
 ]
 const ButtonsTheme = () => {
   return `
     <div class="ui-buttons ui-component">
-      <h2 class="ui-lead py-2">Buttons</h2>
-      <div class="list grid-md-3">   
+      <h2 class="ui-lead py-2 h2 border-bottom border-2">Buttons</h2>
+      <div class="list grid-md-2 gap-3">   
       ${themedButtons.filter(o=>o.value.length !== 0).map(({label, value})=>{
         return `<div class="item">
-        ${label !== "" ? `<h3 class="ui-heading">${label}</h3>` : ``} 
+        ${label !== "" ? `<h3 class="ui-heading h4 my-2">${label}</h3>` : ``} 
         <p class="color-muted">Variations: | ${value.map(v=>{return v.charAt(0).toUpperCase() + v.slice(1)}).join(", ")}</p>
+        <div class="my-2 p-2 ui-block-code"><pre><<code></code>button type="button" class="btn-fill--${value[0]}">Fill Button<<code></code>/button></pre><pre><<code></code>button type="button" class="btn-border--${value[0]}">Border Button<<code></code>/button></pre></div>
         <button class="btn-fill--${value[0]}" data-role="theme-button" data-list="${value.join(",")}">Fill Button</button>
         <button class="btn-border--${value[0]}" data-role="theme-button" data-list="${value.join(",")}">Border Button</button>
         </div>
         `
-      })}   
+      }).join("")}   
       ${themedButtonsWithIcons.filter(o=>o.value.length !== 0).map(({label, value})=>{
         return `<div class="item">
-        ${label !== "" ? `<h3 class="ui-heading">${label}</h3>` : ``} 
+        ${label !== "" ? `<h3 class="ui-heading h4 my-2">${label}</h3>` : ``} 
         <p class="color-muted">Variations: | ${value.map(v=>{return v.charAt(0).toUpperCase() + v.slice(1)}).join(", ")}</p>
+        <div class="my-2 p-2 ui-block-code"><pre><<code></code>button class="btn-fill--${value[0]}" icon-position="right"><<code></code>span>Fill Button with Icon<<code></code>/span><<code></code>span class="icon"><<code></code>icon><<code></code>/icon><<code></code>/span><<code></code>/button></pre><pre><<code></code>button class="btn-border--${value[0]}" icon-position="left"><<code></code>span>Border Button with Icon<<code></code>/span><<code></code>span class="icon"><<code></code>icon><<code></code>/icon><<code></code>/span><<code></code>/button></pre></div>
         <button class="btn-fill--${value[0]}" icon-position="right" data-role="theme-button" data-list="${value.join(",")}"><span>Fill Button with Icon</span><span class="icon"><ion-icon name="cloud"></ion-icon></span></button>
         <button class="btn-border--${value[0]}" icon-position="left" data-role="theme-button" data-list="${value.join(",")}"><span>Border Button with Icon</span><span class="icon"><ion-icon name="cloud"></ion-icon></span></button>
         </div>
         `
-      })}   
+      }).join("")}   
       ${themedButtonsOnlyIcons.filter(o=>o.value.length !== 0).map(({label, value})=>{
         return `<div class="item">
-        ${label !== "" ? `<h3 class="ui-heading">${label}</h3>` : ``} \
+        ${label !== "" ? `<h3 class="ui-heading h4 my-2">${label}</h3>` : ``} \
         <p class="color-muted">Variations: | ${value.map(v=>{return v.charAt(0).toUpperCase() + v.slice(1)}).join(", ")}</p>
         <button class="btn-fill--${value[0]} btn-icon" data-role="theme-button" data-list="${value.join(",")}"><ion-icon name="cloud"></ion-icon></button>
         <button class="btn-border--${value[0]} btn-icon" data-role="theme-button" data-list="${value.join(",")}"><ion-icon name="cloud"></ion-icon></button>
         </div>
         `
-      })}   
+      }).join("")}   
 
       ${themedButtonsOnlyIconsColor.filter(o=>o.value.length !== 0).map(({label, value})=>{
         return `<div class="item">
-        ${label !== "" ? `<h3 class="ui-heading">${label}</h3>` : ``} 
+        ${label !== "" ? `<h3 class="ui-heading h4 my-2">${label}</h3>` : ``} 
         <p class="color-muted">Variations: | ${value.map(v=>{return v.charAt(0).toUpperCase() + v.slice(1)}).join(", ")}</p>
         <button class="btn-icon--${value[0]} btn-icon" data-role="theme-button" data-list="${value.join(",")}"><ion-icon name="cloud"></ion-icon></button>
         </div>
         `
-      })}   
+      }).join("")}   
 
-      
-        
-      
+
+
       </div>      
     </div>
+    
   `
+
+
 }
 
 export default ButtonsTheme
--- src/components/styleguide/colors.js
+++ src/components/styleguide/colors.js
@@ -5,8 +5,8 @@ const primaryColors = [
   {"label": "Alert/Notification", "value":[{"color": "alert","text": "light"},{"color": "information","text": "light"}, {"color": "error","text": "light"},{"color": "warning","text": "light"},{"color": "success","text": "light"}]},
   {"label": "Availability", "value": [{"color": "stock-ok","text": "light"}, {"color": "stock-limited","text": "light"}, {"color": "stock-none","text": "light"}]},
   {"label": "Basic", "value": [{"color": "dark","text": "light"}, {"color": "light","text": "dark"}]},  
-  {"label": "Gray Tones", "value": [{"color": "gray-one","text": "dark"}, {"color": "gray-two","text": "dark"}, {"color": "gray-three","text": "dark"}, {"color": "gray-four","text": "dark"}, {"color": "gray-five","text": "light"}, {"color": "gray-six","text": "light"}, {"color": "gray-seven","text": "light"}, {"color": "gray-eight","text": "light"}, {"color": "gray-nine","text": "light"}, {"color": "gray-ten","text": "light"}]},
-  {"label":"Inherited Colors", "value": [{"color": "content","text": "light"},{"color": "link","text": "light"},{"color": "muted","text": "dark"},{"color": "border","text": "dark"},{"color": "neutral","text": "dark"}
+  {"label": "Gray Tones", "value": [{"color": "gray-1","text": "dark"}, {"color": "gray-2","text": "dark"}, {"color": "gray-3","text": "dark"}, {"color": "gray-4","text": "dark"}, {"color": "gray-5","text": "light"}, {"color": "gray-6","text": "light"}, {"color": "gray-7","text": "light"}, {"color": "gray-8","text": "light"}, {"color": "gray-9","text": "light"}, {"color": "gray-10","text": "light"}]},
+  {"label":"Inherited Colors", "value": [{"color": "content","text": "light"},{"color": "link","text": "light"},{"color": "muted","text": "dark"},{"color": "border","text": "dark"},{"color": "neutral","text": "dark"}, {"color": "neutral-light","text": "dark"}
   ]}
 ];
 
@@ -14,11 +14,11 @@ const primaryColors = [
 const ColorTheme = () => {
   return `
     <div class="ui-colors ui-component">
-      <h2 class="ui-lead py-2">Theme Colors</h2>
+      <h2 class="ui-lead py-2 h2 border-bottom border-2">Theme Colors</h2>
       <div class="list grid-md-3">      
         ${primaryColors.map(({label,value})=> {
           return `<div class="item">
-          ${label !== "" ? `<h3 class="ui-heading">${label}</h3>` : ``}          
+          ${label !== "" ? `<h3 class="ui-heading h4 my-2">${label}</h3>` : ``}          
             ${value.map(({color, text}) => {return `<div class="ui-color" style="--color: var(--color-${color}); --text: var(--color-${text})"><span>${color.charAt(0).toUpperCase() + color.slice(1)}</span><small>--color-${color}</small></div>`}).join("")}
             </div>`
         }).join("")}
